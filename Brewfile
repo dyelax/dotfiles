f944fafb4ab3fe4f7fbc9cfdeb6d6022e0188fd3
Matt Cooper <coop@Matts-MacBook-Pro.local>
2020-04-16 15:42:26 -0700

    Added Brefile and setup script

---
+++ Brewfile
@@ -0,0 +1,39 @@
+##
+# Homebrew Setup
+##
+brew "wget"
+brew "node"
+
+##
+# Brew Cask for UI Apps
+##
+tap "homebrew/cask"
+cask_args appdir: "/Applications"
+
+# Browsers
+cask "google-chrome"
+brew "chrome-cli"
+cask "firefox"
+
+# Development
+cask "webstorm"
+cask "pycharm-ce"
+cask "sublime-text"
+cask "visual-studio-code"
+
+# Messaging
+cask "goofy"
+cask "slack"
+
+# Productivity
+cask "notion"
+cask "lastpass"
+mas "Todoist", id: 585829637 # Todoist
+
+# OS
+cask "bettertouchtool"
+mas "Screenie", id: 965442961
+
+# Entertainment
+cask "spotify"
+
---
+++ setup.sh
@@ -0,0 +1,56 @@
+DOTFILES_DIR=${funcsourcetrace[1]%/*};
+
+##
+# Install and setup homebrew
+##
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)";
+brew bundle --file $DOTFILES_DIR/Brewfile;
+
+echo "============================================";
+
+##
+# Setup SSH key and add to github
+##
+echo "Setting up SSH key"
+if [[ -f "~/.ssh/id_rsa" ]]; then
+	echo "~/.ssh/id_rsa already exists. Using pre-existing key.";
+else
+	ssh-keygen -t rsa -b 4096 -C "me@matt.coop";
+fi
+if [[ -f "~/.ssh/config" ]]; then
+	# Config file exists, so don't add anything manually for safety.
+	echo "~/.ssh/config already exists, so not adding Host *. See the following link for instructions on manually adding it:";
+	echo "https://help.github.com/en/enterprise/2.15/user/articles/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent.";
+else
+	# Add id_rsa to the config
+	touch ~/.ssh/config;
+	echo "Host *" >> ~/.ssh/config;
+	echo "\tAddKeysToAgent yes" >> ~/.ssh/config;
+	echo "\tUseKeychain yest" >> ~/.ssh/config;
+	echo "\tIdentityFile ~/.ssh/id_rsa" >> ~/.ssh/config;
+fi
+eval "$(ssh-agent -s)";
+ssh-add -K ~/.ssh/id_rsa;
+pbcopy < ~/.ssh/id_rsa.pub;
+echo "id_rsa.pub copied to clipboard. Add it to https://github.com with the following instructions:";
+echo "https://help.github.com/en/enterprise/2.15/user/articles/adding-a-new-ssh-key-to-your-github-account";
+
+echo "============================================";
+
+##
+# Install oh-my-zsh
+##
+echo "Installing oh-my-zsh";
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
+mv ~/.zshrc ~/.zshrc.bak.setup.$(date +'%Y-%m-%d');  # Make a backup in case there was anything important
+touch ~/.zshrc;
+echo "source $DOTFILES_DIR/.zshrc" >> ~/.zshrc;
+
+# Install a powerline-compatible font
+echo "Installing powerline fonts"
+git clone https://github.com/powerline/fonts.git --depth=1 powerline-fonts
+./powerline-fonts/install.sh
+rm -rf powerline-fonts
+
+echo "============================================";
+
